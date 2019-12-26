343fcf85669800e14ee19e56d500d6505a0c9991
seung min <3235tmdals@gmail.com>
2019-12-26 14:20:03 +0900

    added log item for recycler view

---
+++ app/src/main/java/com/example/socketsample/MainFragment.kt
@@ -0,0 +1,11 @@
+package com.example.socketsample
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment(){
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+    }
+}
---
+++ app/src/main/java/com/example/socketsample/MessageAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.socketsample
+
+import android.R
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MessageAdapter: RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageAdapter.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: MessageAdapter.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    }
+}
